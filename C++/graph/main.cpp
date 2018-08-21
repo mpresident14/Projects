252003d889f31809bc00f9cb70e4bb2d52b2b567
mpresident14 <mcprez10@gmail.com>
2018-08-21 02:29:08 +0100

    working on shortest path, found segfault.

--- C++/graph/main.cpp
+++ C++/graph/main.cpp
@@ -1,6 +1,10 @@
 #include "graph.hpp"
 #include "../testing_program/testing-program.hpp"
 #include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
 
 bool oneLetterDifferent(const string& s, const string& t)
 {
@@ -24,13 +28,20 @@ bool oneLetterDifferent(const string& s, const string& t)
 
 int main()
 {
+  ifstream infile("4_letter_dictionary.txt");
+
   Graph<string, oneLetterDifferent> g;
-  string arr[9] = {"rank", "mink", "mine", "mice", "tank", "nice", "nine", "mint", "rink"};
-  for (string& str: arr) {
-    g.addVertex(str);
+
+  string word;
+  while(getline(infile, word)) {
+    string wordWithoutCarriageReturn = word.substr(0, word.size() - 1);
+    g.addVertex(wordWithoutCarriageReturn);
   }
 
-  for (auto& x : g.getShortestPath("rank", "nine")) {
+  cout << g.numVertices() << endl;
+  forward_list<string> path = g.getShortestPath("RANK", "NINE");
+
+  for (const string& x : path) {
     cout << x << " -> ";
   }
   cout << endl;
