8441950c1edb0709ba49a5358e2318ce8e3bd8f2
Miles <mcprez10@gmail.com>
2019-05-22 23:19:46 -0700

    Clean up combo.py

--- Python/combo.py
+++ Python/combo.py
@@ -1,54 +1,24 @@
-def combo(L, k, data):
-    if k == 1:
-        
-        for item in L[0]:
-            data.append([item])
+import sys
 
-        return data
+def getAllCombosHelper(L, k, result):
+    if k == 1:
+        for item in L[0]:
+            result.append([item])
+        return result
    
     else:
-        
         for i in range(0, len(L[0])-k+1):
+            for item in getAllCombosHelper([L[0][i+1:]], k-1, []):
+                result += [ L[0][i:i+1] + item]
 
-            for item in combo([L[0][i+1:]], k-1, []):
+    return result
 
-                #if len(L[0][i:i+1] + item) == 3:
-                     # if len of this == k, do function
-                print("data before:",data)
-                data += [ L[0][i:i+1] + item]
-                print("data after:",data)
+def getAllCombos(L, k):
+    return getAllCombosHelper([L], k, [])
 
-    return data
-
-# L = []
-# for x in [2,3,4,5,6,7,8,9]:
-#     L += 4*str(x)
-# for x in ['T','J','Q','K','A']:
-#     L += 4*str(x)
-
-# L = [L]
-
-# print(len(combo(L,2)))
-
-
-# def combo(L, k, data):
-    
-#     if k == 1:
-#         temp = []
-#         for item in L[0]:
-#             temp.append([item])
-
-#         return temp
-   
-#     else:
-#         print("L:", L)
-#         for i in range(0, len(L[0])-k+1):
-
-#             for item in combo([L[0][i+1:]], k-1, []):
-#                 # print('\n')
-#                 # print("data before:", data)
-#                 # print("add:", [L[0][i:i+1] + item])
-#                 data += [ L[0][i:i+1] + item]
-#                 # print("data after:",data)
-
-#     return data
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Enter a combo size followed by a list of numbers.")
+    else:
+        L = [int(sys.argv[i]) for i in range(2, len(sys.argv))]        
+        print(getAllCombos(L, int(sys.argv[1])))
