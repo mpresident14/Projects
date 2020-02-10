f09d1a459df1db106ec77a2993292470669e11f4
Miles President <mcprez10@gmail.com>
2020-02-10 14:06:57 -0800

    Basically done.

--- C_C++/asciimation/main.cpp
+++ C_C++/asciimation/main.cpp
@@ -24,34 +24,114 @@ void timer(size_t seconds)
     }
 }
 
+void readySetGo()
+{
+    const auto sleepTime = 500ms;
+    cout << "READY" << flush;
+    this_thread::sleep_for(sleepTime);
+    cout << "." << flush;
+    this_thread::sleep_for(sleepTime);
+    cout << "." << flush;
+    this_thread::sleep_for(sleepTime);
+    cout << "." << flush;
+    this_thread::sleep_for(sleepTime);
+    cout << "SET" << flush;
+    this_thread::sleep_for(sleepTime);
+    cout << "."  << flush;
+    this_thread::sleep_for(sleepTime);
+    cout << "."  << flush;
+    this_thread::sleep_for(sleepTime);
+    cout << "."  << flush;
+    this_thread::sleep_for(sleepTime);
+    cout << "GO!!!" << endl;
+}
+
 bool passwordGame()
 {
-    SlideUp("story/challenge.txt").animate("");
-    cleanup();
+    ScrollLeft("pics/computer.txt").animate();
+    SlideUp("story/password.txt").animate("");
+    SlideUp("story/password_challenge.txt").animate("");
 
+    cleanup();
     system("clear");
-    cout << "START GUESSING!!!" << endl;
+    readySetGo();
 
     string password = "1234";
     random_shuffle(password.begin(), password.end());
-
     auto future = async(launch::async, timer, 30);
-    string guess;
 
+    string guess;
     while (future.wait_for(0ms) != future_status::ready) {
         getline(cin, guess);
-
         if (guess == password) {
             init_ncurses();
             SlideUp("story/access_granted.txt").animate("");
             return true;
+        } else {
+            cout << "Access Denied!"  << '\n' << endl;
         }
     }
 
     init_ncurses();
-    SlideUp("story/access_denied.txt").animate("");
-
+    SlideUp("story/mission_failed.txt").animate("");
     return false;
+}
+
+const char alphanum[] = "            0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+string randomString(size_t len)
+{
+    string s;
+    s.resize(len);
+
+    for (size_t i = 0; i < len; ++i) {
+        // No spaces at beginning or end.
+        char c;
+        do {
+            c = alphanum[rand() % (sizeof(alphanum) - 1)];
+        } while ((i == 0 || i == len - 1) && c == ' ');
+
+        s[i] = c;
+    }
+
+    return s;
+}
+
+bool eavesdropGame()
+{
+    ScrollLeft("pics/phone.txt").animate();
+    SlideUp("story/eavesdrop.txt").animate("");
+    SlideUp("story/eavesdrop_challenge.txt").animate("");
+
+    cleanup();
+    system("clear");
+    readySetGo();
+
+    auto future = async(launch::async, timer, 45);
+    string userInput;
+
+    for (size_t i = 1; i < 6; ++i) {
+        string randText = randomString(i * 5);
+        cout << randText << endl;
+        while (true) {
+            if (future.wait_for(0ms) == future_status::ready) {
+                init_ncurses();
+                SlideUp("story/mission_failed.txt").animate("");
+                return false;
+            }
+            getline(cin, userInput);
+            if (userInput == randText) {
+                cout << "CORRECT!" << '\n' << endl;
+                break;
+            } else {
+                cout << "INCORRECT! TRY AGAIN." << '\n' << endl;
+                cout << randText << endl;
+            }
+        }
+    }
+
+    init_ncurses();
+    SlideUp("story/success.txt").animate("");
+    return true;
 }
 
 int main()
@@ -60,20 +140,24 @@ int main()
     // Set up ncurses so that it's ready to display our asciimation.
     init_ncurses();
 
-    SlideUp("pics/ralph_vs_pikachu.txt").animate("");
-    SlideUp("story/intro.txt").animate("");
+    ScrollLeft("pics/vday_presents.txt").animate();
+
+    // SlideUp("pics/ralph_vs_pikachu.txt").animate("");
+    // SlideUp("story/intro.txt").animate("");
 
     hack_or_spy:
         if (SlideUp("story/hack_or_spy.txt").animate("12") == '1') {
-            ScrollLeft("pics/computer.txt").animate();
-            SlideUp("story/password.txt").animate("");
             if (!passwordGame()) {
                 goto hack_or_spy;
             }
         } else {
-
+            if (!eavesdropGame()) {
+                goto hack_or_spy;
+            }
         }
 
+    SlideUp("story/fantastic_work.txt").animate("");
+    SlideUp("pics/happy_vday.txt").animate("");
 
     // Reset ncurses settings
     cleanup();
